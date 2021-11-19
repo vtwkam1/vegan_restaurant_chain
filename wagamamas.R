9f4eebb68e4faac3e815646319a8b6ec63a9d99a
Vanessa <vkwacko007@gmail.com>
2021-11-19 14:52:42 +0000

    wagamamas first attempt

---
+++ wagamamas.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(xml2)
+library(selectr)
+library(rvest)
+
+options(stringsAsFactors = FALSE)
+require(rvest)
+
+    
+## change Phantom.js scrape file
+url <- "https://deliveroo.co.uk/menu/sheffield/sheffield-city-centre/wagamama-sheffield-city-centre"
+lines <- readLines("scrape_final.js")
+lines[1] <- paste0("var url ='", url ,"';")
+writeLines(lines, "scrape_final.js")
+
+## Download website
+system("phantomjs scrape_final.js")
+
+### use Rvest to scrape the downloaded website.
+html <- read_html("1.html")
+
+class(html)
+
+# extract item name and cost
+item <- html %>% 
+    html_elements(".orderweb__e1b03759 .ccl-1daa0367dee37c3b") %>% 
+    html_text2()
+
+item_name <- item[seq(1, length(item), 2)]
+
+item_price <- item[seq(2, length(item), 2)]
+
+
+# extract item description
+item_description <- html %>% 
+    html_elements(".ccl-91a59d84c8a54641 .ccl-9d0a5327c911d0f3") %>% 
+    html_text2()
+
+# check lengths
+length(item_name)
+length(item_name) == length(item_price)
+length(item_name) == length(item_description)
+
+# create table
+item_table <- tibble(name=item_name, 
+                     description=item_description, 
+                     price=item_price)
+
+item_table <- item_table %>% 
+    filter(!str_detect(name, "bundle")) %>% # drop bundles
+    slice(1:)
+
