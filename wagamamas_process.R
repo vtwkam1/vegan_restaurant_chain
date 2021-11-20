422b3eaebe87ff8675c73ebec172ce339a0e62ef
Vanessa <vkwacko007@gmail.com>
2021-11-20 22:14:26 +0000

    completed wagamamas data extraction

--- wagamamas_process.R
+++ wagamamas_process.R
@@ -2,30 +2,19 @@ library(tidyverse)
 
 source_table <- read.csv("wagamamas.csv")
 
-dim(source_table) # check dimensions
+dim(source_table) # check dimensions (141 rows)
 
 # Simple cleaning
 table <- source_table %>% 
     mutate(across(everything(), str_to_lower)) %>% # convert to lowercase
-    mutate(name = str_replace(name, "new\\!\\s", ""), # remove "new!"
-           price = as.numeric(str_replace(price, "[:symbol:]", ""))) %>%  # price to numeric
-    distinct(name, .keep_all=T) # remove duplicates based on item name
+    mutate(
+           price = as.numeric(str_replace(price, "[:symbol:]", ""))) %>% 
+    filter(between(row_number(), 1, (which(name=="still water")-1))) # drop drinks
 
-# Remove irrelevant items
-table <- table %>% 
-    filter(!str_detect(name, "bundle")) %>% # remove bundles
-    filter(between(row_number(), 1, (which(name=="still water")-1))) %>% # remove drinks
-    filter(!between(row_number(), which(name=="chilli sambal paste (vg)"), which(name=="crispy fish bites (amai sauce)"))) # remove extras, sauces and kids
-
-# Assign item ID
-table <- table %>% 
-    mutate(id = row_number(),
-           .before = 1)
-
-# Assign categories (manual)
+nrow(table) # 115 rows
 
 # Assign menu sections
-menu_section <- read.delim("wagamamas_menu_sections.txt")
+menu_section <- read.delim("wagamamas_menu_sections.txt", header=F)
 menu_section <- as.character(menu_section[,1]) %>% str_to_lower()
 
 ## Tidy menu sections
@@ -33,8 +22,63 @@ menu_section <- menu_section %>%
     str_replace_all("\\<[:graph:]*\\>", "") %>% 
     str_trim()
 
-# Identify vegan options
+## Assign sections (needs updating)
 table <- table %>% 
+    mutate(section = case_when(
+        between(row_number(), which(name=="wagamama rookie bundle"), which(name=="bowls for souls bundle")) ~ menu_section[1],
+        between(row_number(), which(name=="new! spicy teriyaki vegan ‘chicken’ buns (vg)"), which(name=="no duck donburi (vg)")) ~ menu_section[2],
+        between(row_number(), which(name=="vegan sticky 'ribs' (vg)"), which(name=="chicken yakitori")) ~ menu_section[3],
+        between(row_number(), which(name=="new! hoisin pulled duck buns"), which(name=="mixed mushroom hirata steamed buns (vg)")) ~ menu_section[4],
+        between(row_number(), which(name=="steamed chicken (gyoza)"), which(name=="fried prawn (gyoza)")) ~ menu_section[5],
+        between(row_number(), which(name=="salmon harusame salad"), which(name=="tofu harusame salad (vg)")[2]) ~ menu_section[6],
+        between(row_number(), which(name=="katsu curry (chicken)"), which(name=="firecracker curry (tofu) (vg)")[2]) ~ menu_section[7],
+        between(row_number(), which(name=="yaki soba (chicken + prawn) (thin noodles)"), which(name=="ginger chicken udon (thick noodles)")) ~ menu_section[8],
+        between(row_number(), which(name=="grilled chicken ramen"), which(name=="shirodashi ramen (pork belly)")) ~ menu_section[9],
+        between(row_number(), which(name=="teriyaki donburi (chicken)"), which(name=="shu's 'shiok' chicken (under 600 calories)")) ~ menu_section[10],
+        between(row_number(), which(name=="chilli sambal paste (vg)"), which(name=="noodles")) ~ menu_section[11],
+        between(row_number(), which(name=="katsu curry sauce"), which(name=="shichimi")) ~ menu_section[12],
+        between(row_number(), which(name=="mini chicken katsu (katsu curry sauce)"), which(name=="crispy fish bites (amai sauce)")) ~ menu_section[13],
+        between(row_number(), which(name=="white chocolate + ginger cheesecake"), which(name=="white chocolate + ginger cheesecake")) ~ menu_section[14]))
+
+## Identify duplicates
+duplicated <- table %>% 
+    count(name) %>% 
+    filter(n>1)
+
+### check if all duplicates in "the vegan edit"
+table %>% 
+    filter(section=="the vegan edit") %>% 
+    mutate(duplicated = name %in% duplicated$name) # Two items shown as not duplicated due to different naming
+
+section_drop <- c("soulful bundles", "the vegan edit", "extras", "sauces", "kids")
+
+# number of rows dropped when unwanted sections dropped
+table %>% 
+    count(section) %>% 
+    filter(section %in% section_drop) %>% 
+    summarise(sum = sum(n))
+
+table <- table %>% 
+    filter(!section %in% section_drop) %>% # drop sections
+    mutate(name = str_replace(name, "new//!", "") %>% str_trim()) # tidy
+
+nrow(table)
+
+# Assign categories
+table <- table %>% 
+    mutate(category = case_when(
+        section %in% c("gyoza", "hirata steamed buns", "sides") ~ "sides",
+        section %in% c("donburi", "ramen", "teppanyaki", "harusame salad", "curry") ~ "mains",
+        section %in% c("desserts") ~ "desserts"
+    ))
+
+# Assign item ID
+table <- table %>% 
+    mutate(id = row_number(),
+           .before = 1)
+
+# Identify vegan options
+protein_table <- table %>% 
     mutate(vegan = case_when(
         str_detect(name, "(vg)") ~ "vegan",
         str_detect(name, "(v)") ~ "vegetarian",
@@ -44,8 +88,8 @@ table <- table %>%
 veg_protein <- c("seitan", "tofu", "soya")
 
 # Identify vegan proteins
-for (i in vegan_protein) {
-    table <- table %>% 
+for (i in veg_protein) {
+    protein_table <- protein_table %>% 
         mutate("protein.{i}" := if_else(
             ((str_detect(name, i) | str_detect(description, i)) & (vegan %in% c("vegan", "vegetarian"))), 1, 0
         ))
@@ -56,31 +100,56 @@ meat <- c("chicken", "pork", "squid", "prawn", "beef", "salmon", "steak", "cod",
 
 # Identify meat
 for (i in meat) {
-    table <- table %>% 
+    protein_table <- protein_table %>% 
         mutate("protein.{i}" := if_else(
             ((str_detect(name, i) | str_detect(description, i)) & vegan=="non-veg"), 1, 0
         ))
 }
 
 # Temporary table to generate column for identified protein sources
-temp_table <- table %>% 
+temp_table <- protein_table %>% 
     pivot_longer(cols = matches("protein\\."), 
                  names_to = "protein", 
                  names_prefix = "protein\\.",
                  values_to = "binary") %>% 
-    filter(binary==1) %>%
-    select(id, protein)
+    filter(binary==1)
 
 # Join protein source 
-table <- table %>% 
+protein_table <- protein_table %>% 
     select(!matches("protein\\.")) %>% # Dump binary protein columns
-    left_join(temp_table, by="id") # rejoin for pivoted
+    left_join(
+        temp_table %>% select(id, protein), 
+        by="id") # rejoin for pivoted
 
 ## Identify vegan mock meats with unspecified protein source
 meat_string <- paste(meat, collapse="|")
 
-table <- table %>% 
+protein_table <- protein_table %>% 
     mutate(protein = if_else(
         ((str_detect(name, meat_string) | str_detect(description, meat_string)) & (vegan %in% c("vegan", "vegetarian")) & is.na(protein)), "unknown mock meat", protein))
 
-# 
+# Protein SQL table
+protein_sql <- protein_table %>% 
+    select(id, protein) %>%
+    drop_na(protein) %>% 
+    rename(item_id = id)
+
+write.csv(protein_sql, "wagamamas_protein.csv", row.names = F)
+
+# Menu section SQL table
+section_sql <- table %>% 
+    distinct(section) %>% 
+    mutate(section_id = row_number(),
+           .before = 1)
+
+write.csv(section_sql, "wagamamas_section.csv", row.names = F)
+
+# Item SQL table
+item_sql <- table %>% 
+    left_join(section_sql, 
+              by="section") %>% 
+    select(!section) %>% 
+    rename(item_id = id, item_name = name)
+
+write.csv(item_sql, "wagamamas_item.csv", row.names = F)
+
